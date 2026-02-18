afa298827ba42616584c7708f62ee13da7ece02b
root <root@ip-10-42-217-97.ec2.internal>
2026-02-18 11:08:53 +0000

    task0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""This module contains a functions that adds tow floats"""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of tow float numbers"""
+    return a + b
