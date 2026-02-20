0b0a8dde1d8f9ebe8fd44974ad816d0352278b72
root <root@ip-10-42-173-209.ec2.internal>
2026-02-20 19:16:44 +0000

    task3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that converts a float to a string."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
+
