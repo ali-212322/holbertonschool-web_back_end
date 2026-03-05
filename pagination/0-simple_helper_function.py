b0b8c2d17f95c3a09dc2b7abc9fe911848bdb149
root <root@ip-10-42-59-37.ec2.internal>
2026-03-05 10:14:11 +0000

    Task 0 and readme file

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Simple helper function for pagination."""
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple containing the start and end index
+    corresponding to a pagination range.
+    """
+
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)
